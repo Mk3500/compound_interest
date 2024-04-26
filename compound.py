51c337273400d84127add50dc23d6f333582f552
Mk3500 <mk678059779@gmail.com>
2024-04-26 11:10:58 +0100

    updating readme.md and compound.py

--- compound.py
+++ compound.py
@@ -1,25 +1,23 @@
 
 principal = int(input("enter your principal: "))
-p = principal
-while p <= 0:
-    print("amount can not be negative")
+while principal <= 0:
+    print("amount can not be negative or zero")
     principal = int(input("enter your principal: "))
 
 time =int(input("enter your time: "))
-t = time
-while  t <= 0:
-    print("time can not be negative: ")
+while  time <= 0:
+    print("time can not be negative or zero: ")
     time =int(input("enter your time: "))
 rate = int(input("enter rate : "))
-r = rate/100
-while r <= 0:
+while rate <= 0:
     print("rate can not be negative")
     rate = int(input("enter rate : "))
-amount = p * pow(1+r,t)
-print(f"your capital is ${p}")
-print(f"your time is {t} years")
-print(f"your rate is {r} %")
+amount = principal * pow(1+rate/100, time)
+print(f"your capital is ${principal}")
+print(f"your time is {time} years")
+print(f"your rate is {rate} %")
 print(f"your future value is $ {amount:.2f}")
  
 
- sir my code keep looping any time i type a negative figure
+ #sir my code keep looping any time i type a negative figure
+ # thank you sir my code has been updated now wooo!
